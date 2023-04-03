476814c73fecbb1b91887f210d51b513abcb338f
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-04-02 19:13:25 -0500

    add padding to about me page

--- resume-page/src/components/aboutme.js
+++ resume-page/src/components/aboutme.js
@@ -4,13 +4,13 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return(
-      <div>
-        <h1>About Page</h1>
+      <div class="about-me-container">
+        <h3>About Page</h3>
         <p>
-          Hi there! My name is Shaina, and I'm a 24-year-old Puerto Rican woman who recently moved to Austin Texas, about a year ago. I am a software developer with a passion for technology. I actively participate in hackathons and solving programing challenges for fun.<br></br>
-          As a developer, I thrive on the challenge of solving complex problems and finding creative solutions. I am highly skilled in various programming languages and have experience in developing software applications for both desktop and mobile platforms. <br></br>
-          My experience in hackathons has given me a unique perspective on problem-solving, as I have been able to work on a variety of projects with diverse teams. As a result, I have developed excellent leadership and teamwork skills that allow me to effectively communicate with others and drive successful project outcomes.<br></br>
-          Currently, I am actively looking for a new software development position that will allow me to continue to grow my skills and work on exciting projects. I am excited to bring my experience and expertise to a new team and collaborate with other professionals to create innovative solutions.<br></br>
+          Hi there! My name is Shaina, and I'm a 24-year-old Puerto Rican woman who recently moved to Austin Texas, about a year ago. I am a software developer with a passion for technology. I actively participate in hackathons and solving programing challenges for fun.
+          As a developer, I thrive on the challenge of solving complex problems and finding creative solutions. I am highly skilled in various programming languages and have experience in developing software applications for both desktop and mobile platforms. 
+          My experience in hackathons has given me a unique perspective on problem-solving, as I have been able to work on a variety of projects with diverse teams. As a result, I have developed excellent leadership and teamwork skills that allow me to effectively communicate with others and drive successful project outcomes.
+          Currently, I am actively looking for a new software development position that will allow me to continue to grow my skills and work on exciting projects. I am excited to bring my experience and expertise to a new team and collaborate with other professionals to create innovative solutions.
           Overall, I am a driven, passionate, and skilled software developer who is eager to take on new challenges and make a positive impact on the world. Thank you for taking the time to read about me!
         </p>
       </div>
