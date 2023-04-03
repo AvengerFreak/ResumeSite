ec57bbb971380418905f7024ede2ec8f1a9cd925
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-04-02 20:26:41 -0500

    fix job timeline calculation

--- resume-page/src/components/experience.js
+++ resume-page/src/components/experience.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
-// TODO - fix job timeline calculation
-const calculate_job_timeline = () => {
-  const startDate = new Date('1/1/2020'); // new Date(this.props.startDate);
-  const endDate = new Date(); // (this.props.ongoing == 'true') ? new Date() : new Date(this.props.endDate);
+const calculate_job_timeline = (start, end, isOngoing) => {
+  const startDate = new Date(start);
+  const endDate = (isOngoing == 'true') ? new Date() : new Date(end);
   const years = endDate.getFullYear() - startDate.getFullYear();
   const m = Math.abs(startDate.getMonth() - endDate.getMonth());
   
-  return (years > 0) ? years + " Years " + (m > 0 ? m + " Months " : ""): "";
+  return (years > 0) ? years + " Year" + (years > 1 ? "s " : " ") + (m > 0 ? m + " Month" + (m > 1 ? "s " : " ") : ""): (m > 0 ? m + " Month" + (m > 1 ? "s " : " ") : "");
 }
 
 class Experience extends Component {
@@ -16,7 +15,7 @@ class Experience extends Component {
     return(
       <Grid>
         <Cell col={4}>
-          <p>{calculate_job_timeline()}</p>
+          <p>{calculate_job_timeline(this.props.startDate, this.props.endDate, this.props.ongoing)}</p>
         </Cell>
         <Cell col={8}>
           <h4 style={{marginTop:'0px'}}>{this.props.jobName}</h4>
