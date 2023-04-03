785306ffdf0830d077a4adda125e6f6a28da1eff
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-04-02 20:00:25 -0500

    style changes to resume page

--- resume-page/src/components/resume.js
+++ resume-page/src/components/resume.js
@@ -12,7 +12,7 @@ class Resume extends Component {
       <div>
         <Grid>
           <Cell col={4}>
-            <div style={{textAlign: 'center'}}>
+            <div style={{marginTop: '10px'}}>
               <img
                 src="https://media.licdn.com/dms/image/C4D03AQEg3Q797ld7EQ/profile-displayphoto-shrink_800_800/0/1590846307253?e=1685577600&v=beta&t=emeC1HwazgIVtQt-_5Z8E9GWO_FAKqKGZojE_8dRkPQ"
                 alt="avatar"
@@ -20,16 +20,15 @@ class Resume extends Component {
                  />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Shaina Munoz</h2>
-            <h4 style={{color: 'grey'}}>Full Stack Developer</h4>
+            <h2 style={{marginTop: '40px', marginBottom: '40px'}}>Shaina Munoz</h2>
+            <h4 style={{color: 'grey'}}>Objective:</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <p>
-              Hi there! My name is Shaina, and I'm a 24-year-old Puerto Rican woman who recently moved to Austin Texas, about a year ago. I am a software developer with a passion for technology. I actively participate in hackathons and solving programing challenges for fun.<br></br>
-              As a developer, I thrive on the challenge of solving complex problems and finding creative solutions. I am highly skilled in various programming languages and have experience in developing software applications for both desktop and mobile platforms. <br></br>
-              My experience in hackathons has given me a unique perspective on problem-solving, as I have been able to work on a variety of projects with diverse teams. As a result, I have developed excellent leadership and teamwork skills that allow me to effectively communicate with others and drive successful project outcomes.<br></br>
-              Currently, I am actively looking for a new software development position that will allow me to continue to grow my skills and work on exciting projects. I am excited to bring my experience and expertise to a new team and collaborate with other professionals to create innovative solutions.<br></br>
-              Overall, I am a driven, passionate, and skilled software developer who is eager to take on new challenges and make a positive impact on the world. Thank you for taking the time to read about me!
+              I am actively looking for an oportunity continue to growing my skills and work on exciting new projects. I am excited to bring my experience and expertise to a new team and collaborate with other professionals to create innovative solutions.
             </p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <br></br>
+            <h4 style={{color: 'grey'}}>General Information:</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <h5>Address</h5>
             <p>Wells Branch, Austin Texas 78728</p>
